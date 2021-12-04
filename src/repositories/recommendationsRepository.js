9a0aa8bb254afa363a40cb2f1f166bcac2504aeb
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-04 00:15:13 -0300

    Create a get random song function

--- src/repositories/recommendationsRepository.js
+++ src/repositories/recommendationsRepository.js
@@ -1,4 +1,4 @@
-import connection from '../database';
+import connection from '../database.js';
 
 async function insertSong(body) {
     const { name, youtubeLink } = body;
@@ -18,8 +18,26 @@ async function deleteSong(id) {
     await connection.query('DELETE FROM musics WHERE id = $1;', [id]);
 }
 
+async function selectSongs(params) {
+    const amount = params?.amount;
+    const scoreRange = params?.scoreRange;
+
+    let query = 'SELECT id, name, link as "youtubeLink", score FROM musics';
+
+    if (scoreRange === 'bad') query += ' WHERE score <= 10';
+    if (scoreRange === 'good') query += ' WHERE score > 10';
+
+    if (amount) query += ` ORDER BY score ASC LIMIT ${amount}`;
+
+    const result = await connection.query(`${query};`);
+    const songs = result.rows;
+
+    return songs;
+}
+
 export {
     insertSong,
     updateSongScore,
     deleteSong,
+    selectSongs,
 };
