55ac71a2d7a1c8f9c5097829bf3b533d38b0b121
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-03 21:20:56 -0300

    Solve eslint error

--- src/services/recommendationsService.js
+++ src/services/recommendationsService.js
@@ -4,7 +4,7 @@ function newSongValidationError(body) {
     const songSchema = joi.object({
         name: joi.string().trim().min(1).max(100)
             .required(),
-        youtubeLink: joi.string().regex(/(?:https?:\/\/)?(?:www\.|m\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[^&\s\?]+(?!\S))\/)|(?:\S*v=|v\/)))([^&\s\?]+)/).required(),
+        youtubeLink: joi.string().regex(/(?:https?:\/\/)?(?:www\.|m\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[^&\s?]+(?!\S))\/)|(?:\S*v=|v\/)))([^&\s?]+)/).required(),
     });
 
     const { error } = songSchema.validate(body);
