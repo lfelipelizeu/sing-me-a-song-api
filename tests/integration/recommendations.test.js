9360657f205d4d523c94f45a71e845dcc8301984
Luis Felipe <l_felipelizeu@hotmail.com>
2021-12-03 22:08:51 -0300

    Create automated integration tests to the upvote function

--- tests/integration/recommendations.test.js
+++ tests/integration/recommendations.test.js
@@ -58,3 +58,35 @@ describe('POST /recommendations', () => {
         expect(status).toEqual(201);
     });
 });
+
+describe('POST /recommendations/:id/upvote', () => {
+    const song = createSong();
+
+    beforeAll(async () => {
+        const result = await connection.query('INSERT INTO musics (name, link) VALUES (\'Henrique e Juliano - Colega de Caso\', \'https://www.youtube.com/watch?v=6B5-c0EgvC4\') RETURNING *;');
+        song.id = result.rows[0].id;
+    });
+
+    afterAll(async () => {
+        await connection.query('DELETE FROM musics;');
+    });
+
+    it('return 400 for a string on id', async () => {
+        const id = faker.random.word();
+        const result = await agent.post(`/recommendations/${id}/upvote`);
+        const { status } = result;
+        expect(status).toEqual(400);
+    });
+
+    it('return 404 for invalid song id', async () => {
+        const result = await agent.post(`/recommendations/${faker.datatype.number()}/upvote`);
+        const { status } = result;
+        expect(status).toEqual(404);
+    });
+
+    it('return 200 for valid song id', async () => {
+        const result = await agent.post(`/recommendations/${song.id}/upvote`);
+        const { status } = result;
+        expect(status).toEqual(200);
+    });
+});
