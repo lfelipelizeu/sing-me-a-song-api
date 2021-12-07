018a237579ed33a5d01e7780564acdc603412aca
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-07 15:52:19 -0300

    preparando para o review

--- src/server.js
+++
@@ -1,9 +0,0 @@
-import './setup.js';
-import app from './app.js';
-
-const port = 4000;
-
-app.listen(port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Server running on port ${port}`);
-});
--- src/setup.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-let envFile = '.env';
-if (process.env.NODE_ENV === 'test') envFile = '.env.test';
-if (process.env.NODE_ENV === 'dev') envFile = '.env.dev';
-
-dotenv.config({
-    path: envFile,
-});
